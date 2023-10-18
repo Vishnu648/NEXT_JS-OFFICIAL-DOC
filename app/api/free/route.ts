df38654702bd1629d09fffcf242b2edec2cb9470
VishnuVS <vishnusenthil222@gmail.com>
2023-10-18 20:15:43 +0530

    api route route

---
+++ app/api/free/route.ts
@@ -0,0 +1,12 @@
+export async function GET() {
+  const res = await fetch('https://epic-free-games.p.rapidapi.com/epic-free-games', {
+    headers: {
+      'X-RapidAPI-Key': 'd99907482cmsh9ab0fc66bc0daf3p153337jsn95f51e31894e',
+      'X-RapidAPI-Host': 'epic-free-games.p.rapidapi.com'
+    },
+  })
+  const data = await res.json()
+  console.log(data)
+ 
+  return Response.json({ data })
+}
--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,6 @@
-export default async function GET() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos', {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    const data = await res.json()
-   
-    return Response.json({ data })
+export async function GET() {
+
+    console.log("main Route- Hello, World!");
+    return Response.json({ message: "main Route- Hello, World!" });
   }
+  
